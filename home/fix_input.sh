9682904e2a7519309e02fa3770145acfc2fe17b8
Matt Ziegelbaum <ziegs@google.com>
2017-12-19 01:35:10 -0500

    Cleanup fix_input script

--- home/fix_input.sh
+++ home/fix_input.sh
@@ -5,35 +5,7 @@ xkbcomp -w0 -I$HOME/.xkb -R$HOME/.xkb keymap/mykbd $DISPLAY
 trackpoint="TPPS/2 IBM TrackPoint"
 touchpad="SynPS/2 Synaptics TouchPad"
 
-# Enable vertical scrolling for TrackPoint
-xinput set-prop "$trackpoint" "Evdev Wheel Emulation" 1
-xinput set-prop "$trackpoint" "Evdev Wheel Emulation Button" 2
-xinput set-prop "$trackpoint" "Evdev Wheel Emulation Timeout" 200
-
-# Mouse sensitivity
-xinput set-prop "$trackpoint" "Device Accel Constant Deceleration" 0.35
-xinput set-prop "$touchpad" "Device Accel Constant Deceleration" 1.25
-
-# Disable touchpad right click
-synclient RightButtonAreaLeft=0
-synclient RightButtonAreaTop=0
-
-# Reverse scrolling
-synclient VertTwoFingerScroll=1
-synclient VertScrollDelta=-114
-synclient HorizTwoFingerScroll=1
-synclient HorizScrollDelta=-114
-
-# Turn on palm detect (doesn't really work :/)
-synclient PalmDetect=1
-
-# Three finger click
-synclient TapButton3=2
-
-# Run syndaemon if it's not already running
-sdpid=`ps -ef|grep -v grep|grep syndaemon|awk '{print $2}'`
-if [ -z $sdpid ]
-then
-  echo "Starting syndaemon"
-  syndaemon -i 1 -K -d
-fi
+xinput set-prop "$touchpad" "libinput Natural Scrolling Enabled" 1 # correct scrolling direction
+xinput set-prop "$touchpad" "libinput Tapping Enabled" 1 # tap-to-click
+xinput set-prop "$touchpad" "libinput Click Method Enabled" 0, 1 # two-finger right click
+xinput set-prop "$touchpad" "libinput Accel Speed" 1.0 # fastest, doesn't feel as fast as I want it, figuring it out
